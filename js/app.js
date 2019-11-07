087e8941ebaeeb9a9f65a1dc6bce611412f342ae
Leah H <harrichl@gmail.com>
2019-11-06 19:08:07 -0500

    add getRecipe function to get full recipe info. still to do: display ingredients list

--- js/app.js
+++ js/app.js
@@ -4,10 +4,37 @@ document.addEventListener("DOMContentLoaded", function () {
         var recipeHTML = recipesArray.map(currentRecipe => {
             return ` 
                 <div class="card recipe">
-                    <img class="card-img-top" src="${currentRecipe.image}" alt="movie poster">
+                    <img class="card-img-top" src="${currentRecipe.image}" alt="recipe image">
                     <div class="card-body">
                         <h5 class="card-title">${currentRecipe.title}</h5>
                         <p class="card-likes">Likes: ${currentRecipe.likes}</p>
+                                <!-- Button trigger modal -->
+                <button class="butn" data-toggle="modal" data-target="#exampleModalLong" id="recipe-info" onclick="getRecipe(${currentRecipe.id})">
+                  See recipe
+                </button>
+                
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      
+                      <div class="modal-body"> 
+                      <div id="card-recipe-image"></div>
+                      <p id="card-recipe-instructions"></p>
+                      <div id="card-recipe-ingredients"></div>
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                     </div>
                  </div>       
                 `
@@ -20,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("search-form").addEventListener("submit", function (e) {
         e.preventDefault();
 
-        var searchString = document.getElementById('search-bar').value.split(" ").join("").toLowerCase();
+        const searchString = document.getElementById('search-bar').value.split(" ").join("").toLowerCase();
 
         axios({
             "method":"GET",
@@ -38,14 +65,14 @@ document.addEventListener("DOMContentLoaded", function () {
         })
             .then((response)=>{
 
+                console.log(response);
+
                 recipeData = response.data;
 
-                var recipesContainer = document.getElementsByClassName("results")[0];
-
-                var recipeHTML = renderRecipes(recipeData);
-                recipesContainer.innerHTML = recipeHTML;
+                const recipesContainer = document.getElementsByClassName("results")[0];
 
                 renderRecipes(recipeData);
+
 
             })
             .catch((error)=>{
@@ -53,8 +80,42 @@ document.addEventListener("DOMContentLoaded", function () {
             })
     });
 
-
 });
+
+function getRecipe(id) {
+
+    axios({
+        "method":"GET",
+        "url":`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
+        "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "x-rapidapi-key":"d64ead36d4msh2d7c35ad33b43ddp1579e9jsnec14e0565611"
+        }
+    })
+        .then((response)=>{
+            console.log(response);
+
+            const recipeTitle = document.getElementById('exampleModalLongTitle');
+            const recipeImage = document.getElementById('card-recipe-image');
+            const recipeInstructions = document.getElementById('card-recipe-instructions');
+            const recipeIngredients = document.getElementById('card-recipe-ingredients');
+
+
+            recipeTitle.innerText = `${ response.data.title }`;
+            recipeImage.innerHTML = `<img src="${response.data.image}" alt="recipe image">`;
+            recipeInstructions.innerText = `Instructions: ${ response.data.instructions}`;
+            recipeIngredients.innerHTML = `Ingredients: $ ${response.data.extendedIngredients}`;
+
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+}
+
+
+
+
 
 
 
