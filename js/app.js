7893a370a80ade75fe4895e839c4041c86cf1737
Natalie Cataldo <nataliecataldo@Natalies-MacBook-Pro.local>
2019-11-16 13:16:52 -0500

    Committing the files of natalies changes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
+let currentRecipeResults = [];
 
 document.addEventListener("DOMContentLoaded", function () {
-
     function renderRecipes(recipesArray) {
         var recipeHTML = recipesArray.map(currentRecipe => {
             return ` 
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     <img class="card-img-top" src="${currentRecipe.image}" alt="recipe image">
                         <div class="card-body">
                             <h5 class="card-title">${currentRecipe.title}</h5>
-                            <p class="card-likes">Likes: ${currentRecipe.likes}</p>
+                            <p class="card-likes">Likes: ${currentRecipe.aggregateLikes}</p>
                                 <!-- Button trigger modal -->
                         <button class="butn" data-toggle="modal" data-target="#exampleModalLong" id="recipe-info" onclick="getRecipe(${currentRecipe.id})">
                         See recipe
@@ -24,19 +24,16 @@ document.addEventListener("DOMContentLoaded", function () {
                                         <span aria-hidden="true">&times;</span>
                                         </button>
                                     </div>
-
                                     <div class="modal-body"> 
                                         <div id="card-recipe-image"></div>
                                         <div id="card-recipe-add-info"></div>               
                                         <p id="card-recipe-instructions"></p>
                                         <div id="card-recipe-ingredients"></div>
                                     </div>
-
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-secondary" id="save-to-faves">Save to Favorites</button>
                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                     </div>
-
                                 </div>
                             </div>
                             <!-- end modal -->    
@@ -44,167 +41,146 @@ document.addEventListener("DOMContentLoaded", function () {
                     </div>
                 </div>    
                 `
-
         }).join("");
 
         document.getElementsByClassName("results")[0].innerHTML = recipeHTML;
     }
-
-    document.getElementById("search-form").addEventListener("submit", function (e) {
+    // in order to use await, you have to use an ASYNC FUNCTION
+    document.getElementById("search-form").addEventListener("submit", async function (e) {
         e.preventDefault();
-
         const searchString = document.getElementById('search-bar').value.split(" ").join(",").toLowerCase();
-        console.log(searchString);  
-
-        axios({
-            "method":"GET",
-            "url":"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients",
-            "headers":{
-                "content-type":"application/octet-stream",
-                "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-                "x-rapidapi-key":"d64ead36d4msh2d7c35ad33b43ddp1579e9jsnec14e0565611"
-            },"params":{
-                "number":"12",
-                "ranking":"1",
-                "ignorePantry":"false",
-                "ingredients":`${searchString}`,
-                "instructionsRequired":"true",
-
+        const searchResults = await axios({
+            "method": "GET",
+            "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients",
+            "headers": {
+                "content-type": "application/octet-stream",
+                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                "x-rapidapi-key": "d64ead36d4msh2d7c35ad33b43ddp1579e9jsnec14e0565611"
+            },
+            "params": {
+                "number": "12",
+                "ranking": "1",
+                "ignorePantry": "false",
+                "ingredients": `${searchString}`,
             }
+        });
 
+        //holds an array of all the id's from the result
+        const idList = [];
+
+        searchResults.data.forEach(searchResult => {
+            idList.push(searchResult.id)
+        });
+
+        // turns an array into a list of values seperated by commas
+        const recipeParams = idList.join();
+
+        const listOfRecipes = await axios({
+            "method": "GET",
+            "url": `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/informationBulk`,
+            "headers": {
+                "content-type": "application/octet-stream",
+                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                "x-rapidapi-key": "d64ead36d4msh2d7c35ad33b43ddp1579e9jsnec14e0565611"
+            },
+            "params": {
+                "ids": `${recipeParams}`
+            }
+        });
+
+        // removes recipes with empty instructions and steps
+        const fileredRecipes = listOfRecipes.data.filter(recipe => {
+            return recipe.analyzedInstructions[0] !== undefined &&
+                recipe.analyzedInstructions[0].steps.length > 0 &&
+                recipe.extendedIngredients.length > 0;
+        });
         
-        })
-            .then((response)=>{
-                console.log(response)
+        // sorts the likes
+        const sortedRecipe = fileredRecipes.sort(compareValues('aggregateLikes', 'desc'));
+        
 
-                recipeData = response.data;
-
-                const sortedRecipe = recipeData.sort(compareValues('likes', 'desc'));
-
-                const recipesContainer = document.getElementsByClassName("results")[0];
-
-                renderRecipes(sortedRecipe);
-
-            })
-            .catch((error)=>{ 
-                // insert conditional logic here? or a continue statement?
-                console.log(error)
-            })
-    });
-
+        // GLOBAL VARIABLE to access recipe data based on id
+        currentRecipeResults = sortedRecipe;
+        // const recipesContainer = document.getElementsByClassName("results")[0];
+        renderRecipes(sortedRecipe)
+    })
 });
 
+        // finds results based on id passed in
 function getRecipe(id) {
-    axios({
-        "method":"GET",
-        "url":`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`,
-        "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":"spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-            "x-rapidapi-key":"d64ead36d4msh2d7c35ad33b43ddp1579e9jsnec14e0565611"
-        }
-    })
-        .then((response)=>{
-            console.log(response);
-            recipe = response.data;
-
-            const recipeTitle = document.getElementById('exampleModalLongTitle');
-            const recipeImage = document.getElementById('card-recipe-image');
-            const recipeAddInfo = document.getElementById('card-recipe-add-info');
-            const recipeInstructions = document.getElementById('card-recipe-instructions');
-            const recipeIngredients = document.getElementById('card-recipe-ingredients');
-
-            let recipeLikesCount = response.data.aggregateLikes;
-            let recipeLikes = document.createElement('p');
-            recipeLikes.innerHTML = `Likes: ${ recipeLikesCount }`;
-
-            let recipeServingsCount = response.data.servings;
-            let recipeServings = document.createElement('p');
-            // recipeServings.innerHTML = `Yield: ${ recipeServingsCount } servings`;
-
-            recipeAddInfo.innerHTML = `Yield: ${recipeServingsCount} servings <br> Likes: ${ recipeLikesCount }`;
-
-            let result = response.data.extendedIngredients.map(({ originalString }) => originalString);
-            let instructions = response.data.analyzedInstructions[0].steps;
-
-            recipeTitle.innerText = `${ response.data.title }`;
-            recipeImage.innerHTML = `<img src="${response.data.image}" alt="recipe image">`;
-            recipeInstructions.innerText = `${ response.data.instructions}`;
-
-            let instructionsList = document.createElement('ol');
-            instructions.forEach(instruction => {
-                let li = document.createElement('li');
-                instructionsList.appendChild(li);
-                li.innerHTML += instruction.step;
-            });
-
-            recipeInstructions.innerText = 'Preparation: ';
-            recipeInstructions.appendChild(instructionsList);
-
-            let recipeList = document.createElement('ul');
-
-            result.forEach(ingredient => {
-                let li = document.createElement('li');
-                recipeList.appendChild(li);
-                li.innerHTML += ingredient;
-            });
-
-            recipeIngredients.innerText = 'Ingredients: ';
-            recipeIngredients.appendChild(recipeList);
-
-            saveToFaves_Btn = document.getElementById('save-to-faves');
-            saveToFaves_Btn.addEventListener('click', function(e) {
-                e.preventDefault();
-
-                var firebaseRef = firebase.database().ref('recipes');
-                data = {
-                    title: `${response.data.title}`,
-                    image: `<img src="${response.data.image}" alt="recipe image">`,
-                    likes: recipeLikesCount,
-                    yield: recipeServingsCount,
-                    instructions: instructions,
-                    ingredients: result
-                };
-
-                firebaseRef.push().set(data);
-            });
-
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+    const response = currentRecipeResults.find(result => result.id = id);
+    const recipeTitle = document.getElementById('exampleModalLongTitle');
+    const recipeImage = document.getElementById('card-recipe-image');
+    const recipeAddInfo = document.getElementById('card-recipe-add-info');
+    const recipeInstructions = document.getElementById('card-recipe-instructions');
+    const recipeIngredients = document.getElementById('card-recipe-ingredients');
+    let recipeLikesCount = response.aggregateLikes;
+    let recipeLikes = document.createElement('p');
+    recipeLikes.innerHTML = `Likes: ${recipeLikesCount}`;
+    let recipeServingsCount = response.servings;
+    let recipeServings = document.createElement('p');
+    
+    // recipeServings.innerHTML = `Yield: ${ recipeServingsCount } servings`;
+    recipeAddInfo.innerHTML = `Yield: ${recipeServingsCount} servings <br> Likes: ${recipeLikesCount}`;
+    let result = response.extendedIngredients.map(({ originalString }) => originalString);
+    let instructions = response.analyzedInstructions[0].steps;
+    recipeTitle.innerText = `${response.title}`;
+    recipeImage.innerHTML = `<img src="${response.image}" alt="recipe image">`;
+    recipeInstructions.innerText = `${response.instructions}`;
+    let instructionsList = document.createElement('ol');
+    instructions.forEach(instruction => {
+        let li = document.createElement('li');
+        instructionsList.appendChild(li);
+        li.innerHTML += instruction.step;
+    });
+    recipeInstructions.innerText = 'Preparation: ';
+    recipeInstructions.appendChild(instructionsList);
+    let recipeList = document.createElement('ul');
+    result.forEach(ingredient => {
+        let li = document.createElement('li');
+        recipeList.appendChild(li);
+        li.innerHTML += ingredient;
+    });
+    recipeIngredients.innerText = 'Ingredients: ';
+    recipeIngredients.appendChild(recipeList);
+    saveToFaves_Btn = document.getElementById('save-to-faves');
+    saveToFaves_Btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        var firebaseRef = firebase.database().ref('recipes');
+        data = {
+            title: `${response.title}`,
+            image: `<img src="${response.image}" alt="recipe image">`,
+            likes: recipeLikesCount,
+            yield: recipeServingsCount,
+            instructions: instructions,
+            ingredients: result
+        };
+        firebaseRef.push().set(data);
+    });
 }
-
 
 function saveRecipe(id) {
     console.log(recipe, id, "id");
-
     let recipeListJSON = localStorage.getItem('recipeList');
     let recipeList = JSON.parse(recipeListJSON);
-        if (recipeList == null) {
-            recipeList= [];
-        } 
-    recipeList.push(recipe); 
+    if (recipeList == null) {
+        recipeList = [];
+    }
+    recipeList.push(recipe);
     recipeButton = document.getElementById('saveButton');
     recipeButton.innerHTML = "Saved!";
     recipeButton.className = ("disabled");
-
     recipeListJSON = JSON.stringify(recipeList);
     localStorage.setItem('recipeList', recipeListJSON);
 }
-
-
-
-function compareValues(key, order='asc') {
-    return function(a, b) {
-        if(!a.hasOwnProperty(key) ||
+function compareValues(key, order = 'asc') {
+    return function (a, b) {
+        if (!a.hasOwnProperty(key) ||
             !b.hasOwnProperty(key)) {
             return 0;
         }
-
         const varA = a[key];
         const varB = b[key];
-
         let comparison = 0;
         if (varA > varB) {
             comparison = 1;
@@ -217,35 +193,15 @@ function compareValues(key, order='asc') {
         );
     };
 }
-
 getRecipeFaves_Btn = document.getElementById('get-recipe-faves-list');
 getRecipeFaves_Btn.addEventListener('click', function (e) {
     window.alert('You clicked me!');
     e.preventDefault();
-
     var rootRef = firebase.database().ref().child('recipes');
     rootRef.on('child_added', snapshot => {
-            snapshot.forEach(function (childSnapshot) {
-                console.log(childSnapshot);
-                // renderRecipes(childSnapshot);
-            })
-        });
-
-
+        snapshot.forEach(function (childSnapshot) {
+            console.log(childSnapshot);
+            // renderRecipes(childSnapshot);
+        })
+    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
